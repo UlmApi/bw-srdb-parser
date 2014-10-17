1170b55cae94e7cf4f713141ab4df5734c0d356c
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2014-10-17 17:27:20 +0200

    create getall, parses 'H' and 'U' sections --> Table IDs extracted

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ bw-srdb-parser
 ==============
 
 Parser für http://www.statistik-bw.de/SRDB/ zu $formaten.
+
+
+Dependencies:
+* Developed on Python 2.7.6.
+* PIP resources:
+  * BeautifulSoup HTML parsing library.
+  * requests HTTP library.
+
---
+++ lib/getall.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup as BS
+import requests
+
+liste=[]
+#Get for each H (topic)
+for x in range(1,12):
+  liste.append("http://www.statistik-bw.de/SRDB/home.asp?H="+str(x)+"&E=GE")
+
+liste2=[]
+for x in liste:
+  soup = BS(requests.get(x).text)
+  for y in soup.find(attrs={"name":"U"}).children:
+    soup2 = BS(requests.get(x + "&U=" y.get(value)).text)
+    for z in soup.find(attrs={"name":"T"}).children:
+      liste2=(y + "&T=" z.get(value))
