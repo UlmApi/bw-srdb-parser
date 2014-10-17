faf3e66694a1e489eeb281cdff05f74ddd7c60b9
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2014-10-17 18:24:32 +0200

    fetch and print out a list of table URLs

--- lib/getall.py
+++ lib/getall.py
@@ -7,9 +7,20 @@ for x in range(1,12):
   liste.append("http://www.statistik-bw.de/SRDB/home.asp?H="+str(x)+"&E=GE")
 
 liste2=[]
+found =[]
 for x in liste:
   soup = BS(requests.get(x).text)
   for y in soup.find(attrs={"name":"U"}).children:
-    soup2 = BS(requests.get(x + "&U=" + y.get(value)).text)
-#    for z in soup.find(attrs={"name":"T"}).children:
-#      liste2=(y + "&T=" + z.get(value))
+    if y == u"\n":
+      continue
+    soup2 = BS(requests.get(x + "&U=" + y.get("value")).text)
+    res = soup2.find(attrs={"name":"T"})
+    if not res:
+        print "Warning, did not find any tables for:" + x + "&U=" + y.get("value")
+        continue
+    for z in res.children:
+      if z == u"\n":
+        continue
+      liste2.append(x + "&U=" + y.get("value") + "&T=" + z.get("value"))
+
+print liste2
