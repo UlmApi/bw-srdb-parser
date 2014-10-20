1885398c7ec3c0c8ff067f6b0cefa3608eca6671
Sergej Schmidt <sergej.schmidt@uni-ulm.de>
2014-10-20 19:10:56 +0200

    basic debug logs

--- bw-srdb-parser.py
+++ bw-srdb-parser.py
@@ -2,11 +2,13 @@
 
 from lib import ResponseParser
 import requests
+import logging
 
 ### 
 average_age_link = "http://www.statistik-bw.de/SRDB/Tabelle.asp?H=1&U=02&T=01035100&E=LA&R=LA"
 
 def main():
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     # TODO fetch all links, then get html code from particular   
     rp = ResponseParser.ResponseParser()
     # get html_code 
--- lib/ResponseParser.py
+++ lib/ResponseParser.py
@@ -1,29 +1,36 @@
+import logging
 from bs4 import BeautifulSoup as BS
 
 class ResponseParser(object):
 
-    def __init__(self):
-        print('nothing')
+    #def __init__(self):
+    #    print('nothing')
 
+    """
+    Attempt to write a generic parser for the perverted, non-uniformed html tables.
+    Works with the following URLs
+    @html_code is
+    """
     def generic_parse(self, html_code):
+        ### TODO write data to backend instead into debug messages
         soup = BS(html_code)
         tables = soup.find_all('table')
         table_names = []
         for table in tables:
-            table_names.append(table.get('summary'))
+            table_name = table.get('summary')
+            logging.debug('"Table Name is %s"', table_name)
             # TODO find captions outside tbody
             lines = table.find('tbody').find_all('tr')
             for line in lines:
                 th = line.find('th') 
-                print(th.contents[0].strip())
+                logging.debug('- TH: %s',th.contents[0].strip())
                 tds = line.find_all('td')
                 for td in tds:
-                    print(td.contents[0].strip())
-            if(len(table_names) == 1):
-                print(table_names[0]) 
+                    logging.debug(' ** TD: %s', td.contents[0].strip())
             
 
         #print(table[0].get('summary'))
 
     def parse_average_age(self, html_code):
         self.generic_parse(html_code)
+
