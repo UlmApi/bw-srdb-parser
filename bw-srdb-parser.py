bf178a48286fd66dc57f8e0b500a4070b7b5ab21
Sergej Schmidt <sergej.schmidt@uni-ulm.de>
2014-10-20 19:39:42 +0200

    Table captions (THs) parsed

--- bw-srdb-parser.py
+++ bw-srdb-parser.py
@@ -3,6 +3,7 @@
 from lib import ResponseParser
 import requests
 import logging
+from sys import argv
 
 ### 
 average_age_link = "http://www.statistik-bw.de/SRDB/Tabelle.asp?H=1&U=02&T=01035100&E=LA&R=LA"
@@ -12,8 +13,15 @@ def main():
     # TODO fetch all links, then get html code from particular   
     rp = ResponseParser.ResponseParser()
     # get html_code 
-    av_age = requests.get(average_age_link).text
-    rp.parse_average_age(av_age)
+    
+    # Test with a specific link 
+    #av_age = requests.get(average_age_link).text
+    #rp.parse_average_age(av_age)
+
+    # Test with HTML file and multiple tbodies
+    html_text = open(argv[1], 'r').read()
+    rp.generic_parse(html_text)
+    
 
 if __name__ == "__main__":
     main()
--- lib/ResponseParser.py
+++ lib/ResponseParser.py
@@ -9,28 +9,29 @@ class ResponseParser(object):
     """
     Attempt to write a generic parser for the perverted, non-uniformed html tables.
     Works with the following URLs
-    @html_code is
     """
     def generic_parse(self, html_code):
         ### TODO write data to backend instead into debug messages
         soup = BS(html_code)
         tables = soup.find_all('table')
-        table_names = []
         for table in tables:
             table_name = table.get('summary')
-            logging.debug('"Table Name is %s"', table_name)
-            # TODO find captions outside tbody
-            lines = table.find('tbody').find_all('tr')
-            for line in lines:
-                th = line.find('th') 
+            logging.debug('Table Name is ""%s"', table_name)
+            # TODO parse thead and tfoot for description 
+            trs = table.find('thead').find_all('tr')
+            logging.debug('More captions: ')
+            for tr in trs:
+                ths = tr.find_all('th')
+                for th in ths:
+                    logging.debug(' ** TH: %s', th.contents[0].strip())
+
+            trs = table.find('tbody').find_all('tr')
+            for tr in trs:
+                th = tr.find('th') 
                 logging.debug('- TH: %s',th.contents[0].strip())
-                tds = line.find_all('td')
+                tds = tr.find_all('td')
                 for td in tds:
                     logging.debug(' ** TD: %s', td.contents[0].strip())
             
-
-        #print(table[0].get('summary'))
-
     def parse_average_age(self, html_code):
         self.generic_parse(html_code)
-
